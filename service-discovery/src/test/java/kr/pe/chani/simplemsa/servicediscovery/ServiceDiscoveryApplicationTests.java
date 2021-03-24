f2dab5a905fb52e188130190674eef20c4873dc1
chjung <heeyame@gmail.com>
2021-03-24 14:56:43 +0900

    Simple Eureka Implementation

---
+++ service-discovery/src/test/java/kr/pe/chani/simplemsa/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.chani.simplemsa.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
