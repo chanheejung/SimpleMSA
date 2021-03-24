d61c08a30d29547f86057efeebee0c5bd57d856c
chjung <heeyame@gmail.com>
2021-03-24 16:18:06 +0900

    Simple Service2 Implementation

---
+++ service2/src/main/java/kr/pe/chani/simplemsa/service2/hello/NameController.java
@@ -0,0 +1,22 @@
+package kr.pe.chani.simplemsa.service2.hello;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping(value="/name")
+public class NameController {
+
+    @Autowired
+    NameService nameService;
+
+    @GetMapping(value = "/{id}")
+    public String name(@PathVariable("id") String id) {
+        return nameService.getName(id);
+    }
+}
---
+++ service2/src/main/java/kr/pe/chani/simplemsa/service2/hello/NameService.java
@@ -0,0 +1,20 @@
+package kr.pe.chani.simplemsa.service2.hello;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Service;
+
+@RefreshScope
+@Service
+public class NameService {
+
+    public String getName(String id) {
+        switch (id) {
+            case "1":
+                return "Jesse";
+            case "2":
+                return "Jimmy";
+            default:
+                return "UnKnown";
+        }
+    }
+}
