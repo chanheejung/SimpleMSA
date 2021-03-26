45448524a2d85ff1888609b96b685a1415b36cc5
chjung <heeyame@gmail.com>
2021-03-26 13:58:21 +0900

    Create a Empty Config Server

---
+++ config-server/src/main/java/kr/pe/chani/simplemsa/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.chani.simplemsa.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
---
+++ config-server/src/test/java/kr/pe/chani/simplemsa/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.chani.simplemsa.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
