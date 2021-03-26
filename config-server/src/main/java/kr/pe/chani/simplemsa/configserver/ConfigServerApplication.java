d2d232a77bd84fc2c177e4c61771aaa2dd22cd1a
chjung <heeyame@gmail.com>
2021-03-26 14:52:55 +0900

    Implementing Config Server 1 : File based Properties

--- config-server/src/main/java/kr/pe/chani/simplemsa/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/kr/pe/chani/simplemsa/configserver/ConfigServerApplication.java
@@ -2,7 +2,9 @@ package kr.pe.chani.simplemsa.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
